c16d86f72cb57ba3a8909ef7379609f4741a326c
Anish202020 <anishkumarbdmi@gmail.com>
2024-10-05 08:59:38 +0530

    commit changes

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,14 @@
 import { SignUp } from '@clerk/nextjs';
+import { CardBody, CardContainer } from "@/components/ui/3d-card";
 
 export default function SiginUpPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
-      <SignUp/>
+      <CardContainer className="inter-var">
+      <CardBody className="relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+          <SignUp/>
+      </CardBody>
+    </CardContainer>
     </main>
   );
 }
