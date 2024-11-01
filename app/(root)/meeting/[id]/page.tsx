67e829fdf5ea49088e656e21f7dde152e3ce1918
Anish202020 <anishkumarbdmi@gmail.com>
2024-11-01 10:58:40 +0530

    commit changes

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,6 +1,9 @@
+"use client"
+import { useUser } from '@clerk/nextjs'
 import React from 'react'
 
 const Meeting = ({params}:{params:{id:string}}) => {
+  const user= useUser() 
   return (
     <div>
         <div>Meeting Room: #{params.id}</div>
