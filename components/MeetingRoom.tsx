42514d4eaba509bfe753be3d012056d4a6cb0c16
Anish202020 <anishkumarbdmi@gmail.com>
2024-11-11 22:51:08 +0530

    commit changes

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const MeetingRoom = () => {
   return (
-    <div>
-      Meeting Room
-    </div>
+    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+      <div className='relative flex size-full items-center justify-center'>
+        <div className='flex size-full max-w-[1000px] items-center'>
+          Meeting Room
+        </div>
+      </div>
+    </section>
   )
 }
 
