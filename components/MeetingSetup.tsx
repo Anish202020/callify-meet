cbe706ce05470410008e8a204e9f45bf8044cf36
Anish202020 <anishkumarbdmi@gmail.com>
2024-11-11 22:03:04 +0530

    commit changes

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const MeetingSetup = () => {
   return (
-    <div>
-      
+    <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
+      <h1></h1>
     </div>
   )
 }
