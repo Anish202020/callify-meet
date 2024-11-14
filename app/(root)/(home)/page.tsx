b77ddad92b9c381bbed11a483d07cafc317330a4
Anish202020 <anishkumarbdmi@gmail.com>
2024-11-14 10:16:48 +0530

    npm run dev

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -50,10 +50,10 @@ const Home = () => {
 
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-      {/* <h1 className='text-3xl font-bold'>
+      <h1 className='text-3xl font-bold'>
       Home
-    </h1> */}
-      <Lens hovering={hovering} setHovering={setHovering}>
+    </h1>
+      {/* <Lens hovering={hovering} setHovering={setHovering}>
         <div className='h-[350px] cursor-pointer w-full rounded-[20px] bg-hero bg-cover'>
           <motion.div
             animate={{
@@ -75,9 +75,9 @@ const Home = () => {
           </motion.div>
 
         </div>
-      </Lens>
+      </Lens> */}
 
-      <MeetingTypeList />
+      {/* <MeetingTypeList /> */}
       {/* <h1 className='text-3xl font-bold'>
         Home
       </h1> */}
