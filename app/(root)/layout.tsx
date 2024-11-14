bfa8b5181b20a7a7e9805565af4ccb2d1b1f22d9
Anish202020 <anishkumarbdmi@gmail.com>
2024-11-14 19:29:29 +0530

    commit changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,16 @@
 import React, { ReactNode } from 'react'
 
 import StreamVideoProvider from '@/providers/StreamClientProvider';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Callify",
+  description: "Callify - Video Conferencing App",
+  icons:{
+    icon: "/data/callify.png"
+  }
+};
+
 
 const RootLayout = ({children}:{children:ReactNode}) => {
   return (
