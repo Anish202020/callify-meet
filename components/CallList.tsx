7075ccb40c227bb19a85dde2e0e9e3b6cd86f576
Anish202020 <anishkumarbdmi@gmail.com>
2024-11-22 10:49:23 +0530

    commit changes

---
+++ components/CallList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
+  return (
+    <div>
+      CallList
+    </div>
+  )
+}
+
+export default CallList
