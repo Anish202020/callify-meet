d0fe23299c05c48c5aeedf55ca8833f5222c38c0
Anish202020 <anishkumarbdmi@gmail.com>
2024-11-22 10:27:49 +0530

    commit changes

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -7,6 +7,7 @@ import MeetingModal from './MeetingModal';
 import { useUser } from '@clerk/nextjs';
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useToast } from '@/hooks/use-toast';
+import { Textarea } from './ui/textarea';
 
 
 const MeetingTypeList = () => {
@@ -116,7 +117,45 @@ const MeetingTypeList = () => {
         className="bg-blue-400"
       />
       
+      {!callDetails ? (
+        <MeetingModal
+        isOpen={meetingState==='isScheduleMeeting'}
+        onClose={()=> setMeetingState(undefined)}
+        title="Create Meeting"
+        className="text-center"
+        image="/icons/callify_logo.svg"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      >
+        <div className="flex flex-col gap-2.5">
+            <label className="text-base font-normal leading-[22.4px] text-sky-2">
+              Add a description
+            </label>
+            <Textarea
+              className="border-none bg-dark-2 focus-visible:ring-0 focus-visible:ring-offset-0"
+              onChange={(e) =>
+                setValues({ ...values, description: e.target.value })
+              }
+            />
+          </div>
+      </MeetingModal>
+      ):(
+        <MeetingModal
+        isOpen={meetingState==='isScheduleMeeting'}
+        onClose={()=> setMeetingState(undefined)}
+        title="Meeting Created"
+        className="text-center"
+        image="/icons/callify_logo.svg"
 
+        handleClick={()=>{
+          // navigator.clipboard.writeText(meetingLink)
+          // toast({title:'Link Copied'})
+        }}
+        buttonIcon='/icons/copy.svg'
+        buttonText="Copy Meeting Link"
+        
+      />
+      )}
 
       <MeetingModal
         isOpen={meetingState==='isInstantMeeting'}
