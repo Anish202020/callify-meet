87f93f87100d189e98154cedc88c0be1a2afdf65
Anish202020 <anishkumarbdmi@gmail.com>
2024-11-22 11:43:11 +0530

    commit changes

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -7,6 +7,7 @@ import { useGetCalls } from '@/hooks/useGetCalls';
 import MeetingCard from './MeetingCard';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { toast } from '@/hooks/use-toast';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
   const router = useRouter();
@@ -42,15 +43,21 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
 
   useEffect(() => {
     const fetchRecordings = async () => {
-      const callData = await Promise.all(
-        callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
-      );
+      try{
 
-      const recordings = callData
+        const callData = await Promise.all(
+          callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
+        );
+        
+        const recordings = callData
         .filter((call) => call.recordings.length > 0)
         .flatMap((call) => call.recordings);
-
-      setRecordings(recordings);
+        
+        setRecordings(recordings);
+      }catch(error){
+        toast({title:'Try Again Later !'})
+      }
+      
     };
 
     if (type === 'recordings') {
