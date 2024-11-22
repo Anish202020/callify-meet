7737b5acc43a7bb9e438c6446d24c3a651e28410
Anish202020 <anishkumarbdmi@gmail.com>
2024-11-22 11:06:49 +0530

    commit changes

--- constants/index.ts
+++ constants/index.ts
@@ -67,3 +67,12 @@ export const words = [
   
   "Where Voices Unite"
   ];
+
+
+  export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+  ];
